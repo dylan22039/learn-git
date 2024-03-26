fbe3fca30a14c24d835685c4af22b2f7d8f0379d
dylanwang <zoro30910@gmail.com>
2024-03-27 02:37:40 +0800

    Add: final

--- test.c
+++ test.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 int main(){
     
-    for(int x = 10; x > 0; x--) {
+    for(int x = 0, y = 0; x < 10; x++, y+=2) {
         
-        for (int y = 0; y < x; y++) {
-            printf("  ");
+        for (int j = 1; j < (10-x); j++) {
+            printf("_");
         } 
-        for (int y = 0; y < x-2; y++) {
-            printf("*  ");
+        for (int j = 0; j <= y; j++) {
+            printf("*");
         }
-        puts("");
+        for(int j = 1; j < (10-x); j++){
+            printf("_");
+        }
+        printf("\n");
     }
 
     return 0;
